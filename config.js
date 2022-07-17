faf1da636aee362c5e37dd09e39b73debbd14030
louzanizineddine <louzanizineddine@gmail.com>
2022-07-17 11:33:05 +0100

    project config

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports  = {
+    port: process.env.PORT || 3000,
+    mongoURL: `mongodb+srv://louzanizineddine:louzanizineddine@cluster0.8lgfquo.mongodb.net/?retryWrites=true&w=majority`
+}
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose= require('mongoose')
+const {port , mongoURL} = require('./config.js')
+const app = express();
+
+mongoose
+    .connect(mongoURL , {
+        useUnifiedTopology:true
+    })
+    .then(() => {
+    console.log(`connected to mongo database`)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+app.get('/' , (req  , res) => {
+    res.send("hello")
+})
+
+app.listen(port , () => {
+    console.log(`port is running on ${port}`)
+})
