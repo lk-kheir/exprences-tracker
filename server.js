bba44c7b370f552f91342394cba6bd7f5126a84f
louzanizineddine <louzanizineddine@gmail.com>
2022-07-17 11:40:58 +0100

    protection

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose= require('mongoose')
 const {port , mongoURL} = require('./config.js')
 const app = express();
-
+// test comment
 mongoose
     .connect(mongoURL , {
         useUnifiedTopology:true
